46694bd7421b81d2d14e80d92115a3c77fe98284
Milk Wee <1140484715@qq.com>
2025-07-22 22:07:03 +0800

    阿里云文件服务OSS

--- tlias-web-management/src/main/java/com/itheima/comtroller/UploadController.java
+++ tlias-web-management/src/main/java/com/itheima/comtroller/UploadController.java
@@ -1,8 +1,10 @@
 package com.itheima.comtroller;
 
 import com.itheima.pojo.Result;
+import com.itheima.utils.AliyunOSSOperator;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
@@ -16,10 +18,10 @@ public class UploadController {
 
     private static final Logger log = LoggerFactory.getLogger(UploadController.class);
     /**
-     * 文件上传
+     * 文件上传，本地磁盘存储方案
      * @return
      */
-    @PostMapping("/upload")
+    /*@PostMapping("/upload")
     public Result upload( String name,Integer age, MultipartFile file) throws Exception {
         log.info("接受参数：{},{},{}",name,age,file);
         //获取原始文件名
@@ -32,5 +34,21 @@ public class UploadController {
         file.transferTo(new File("D:/upload/"+newFileName));
         return Result.success();
 
+    }*/
+
+    /**
+     * 阿里云OSS存储方案
+     * @return
+     */
+    @Autowired
+    private AliyunOSSOperator aliyunOSSOperator;
+
+    @PostMapping("/upload")
+    public Result upload( MultipartFile file) throws Exception {
+        log.info("文件上传：{}",file);
+        //将文件交给OSS存储管理
+        String url = aliyunOSSOperator.upload(file.getBytes(),file.getOriginalFilename());
+        log.info("文件上传OSS，url：{}", url);
+        return Result.success(url);
     }
 }
